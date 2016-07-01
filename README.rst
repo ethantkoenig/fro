1fc6c43929cffc2e58c7354782deacbc34b1cdca
Ethan Koenig <ethantkoenig@gmail.com>
2016-06-30 21:17:38 -0700

    Various minors cleanups
    EOF newlines, whitespace in example

--- README.rst
+++ README.rst
@@ -5,3 +5,5 @@ Fro is a Python module for parsing strings and text into Python objects. It
 offers a concise, declarative, and expressive interface for mapping strings to
 data, abstracting away boilerplate code and making it easy to quickly write
 bug-free parsers.
+
+Fro is compatible with Python 2.7+ and Python 3+.
--- examples/texExample.py
+++ examples/texExample.py
@@ -1,7 +1,9 @@
 import fro
 
+
 class TexElement(object):
     pass
+
 
 class TexCommand(TexElement):
     def __init__(self, name, content=None):
@@ -13,12 +15,14 @@ class TexCommand(TexElement):
             return "\%s" % self.name
         return "\%s{%s}" % (self.name, self.content)
 
+
 class TexText(TexElement):
     def __init__(self, text):
         self.text = text
 
     def __str__(self):
         return self.text
+
 
 class TexDocument(object):
     def __init__(self, elements):
@@ -27,8 +31,9 @@ class TexDocument(object):
     def __str__(self):
         return " ".join(str(e) for e in self.elements)
 
+
 # parser for TexText objects
-textp = fro.rgx(r"[^\\\s]+", name="TeX text") | TexText
+textp = fro.rgx(r"[^\\s]+", name="TeX text") | TexText
 
 # parser for TexCommand objects
 cmdbasep = fro.group_rgx(r"\\([^\{\s]+)").get()
