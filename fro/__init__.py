6b5dd408316d5908b994c235d5f221f9a47f5d2c
Ethan Koenig <ethantkoenig@gmail.com>
2016-07-16 21:33:35 -0700

    Move interface to parser.py
    This will avoid circular imports for future features, and is coherent since the public methods of the Parser class are part of fro's interface

--- fro/__init__.py
+++ fro/__init__.py
@@ -1,3 +1,3 @@
-from fro._implementation.interface import alt, comp, group_rgx, nested, rgx, seq, tie
-from fro._implementation.interface import floatp, intp, natp, posintp
+from fro._implementation.parser import alt, comp, group_rgx, nested, rgx, seq, tie
+from fro._implementation.parser import floatp, intp, natp, posintp
 from fro._implementation.parse_error import FroParseError
